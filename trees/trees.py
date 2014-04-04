c455a6594a5a3113f6c1bca65a9deba959541e2a
Shmelnick <Shmelnick@mail.ru>
2014-04-04 22:30:37 +0400

    c1

--- trees/trees.py
+++ trees/trees.py
@@ -1,17 +1,65 @@
+""" This module contains some utility procedures for classifier
+Main algorithm: you need to create Classifier with desired parameters (build_classifier.__doc__),
+choose targets of CSV-file (choose_targets.__doc__),
+pre-process CSV-file with features and targets (read_data_set.__doc__),
+Classifier.fit: build classification tree with pre-processed data,
+Classifier.predict
+"""
 import StringIO
 import argparse
 import numpy
 import scipy.stats
-from sklearn import tree
-from sklearn.cross_validation import cross_val_score
 import sklearn.feature_extraction
-from sklearn.tree import DecisionTreeClassifier
 import pydot
+from myTree import MyClassifier
+from myTree import BinaryTree
 
 TARGET = "target"
 
 
-def read_data_set(ds_path, columns):
+def choose_targets(source=2):
+    """
+    0 = VK - CSV - file
+    1 = DT Tutorial (Seminar task)
+    2 = VK
+    :return: chosen columns for classification
+    """
+    if source == 0:
+        columns = {
+            "status_len": (5, parse_int),
+            "m_friends": (10, parse_int),
+            "sex": (2, parse_int),
+            "rel": (4, parse_int),
+            "emo": (6, parse_int),
+            TARGET: (3, parse_int)
+        }
+        return columns
+    elif source == 1:
+        columns = {
+            "age": (4, parse_int),
+            "gender": (5, parse_int),
+            "education": (29, parse_string),
+            TARGET: (54, parse_int)
+        }
+        return columns
+    elif source == 2:
+        columns = {
+            "friends": (15, parse_int),
+            "grad_year": (18, parse_int),
+            "emotions": (6, parse_int),
+            "albums": (14, parse_int),
+            TARGET: (3, parse_int)
+        }
+        return columns
+
+
+def read_data_set(columns, ds_path='data/data.csv', with_targets=True):
+    """ Parse CSV-file, choose no-None data
+
+    :param ds_path: CSV-file path
+    :param columns: returned from choose_targets
+    :return: x(n x m), y(n x 1), feature_names
+    """
     data = []
     targets = []
     with open(ds_path, 'rU') as ds_file:
@@ -31,9 +79,14 @@ def read_data_set(ds_path, columns):
                 else:
                     row[name] = value
 
-            if row_valid and row and target:
-                data.append(row)
-                targets.append(target)
+            if with_targets:
+                if row_valid and row and target:
+                    data.append(row)
+                    targets.append(target)
+            else:
+                # For clf.predict()
+                if row_valid and row:
+                    data.append(row)
 
     dv = sklearn.feature_extraction.DictVectorizer()
     return dv.fit_transform(data).todense(), numpy.array(targets), dv.get_feature_names()
@@ -54,21 +107,13 @@ def print_set_stats(ds, target, feature_names):
     print_distribution(target)
 
 
-def fit_decision_tree(x, y):
-    model = DecisionTreeClassifier(criterion="entropy", min_samples_leaf=1000, max_depth=2)
-
-    scores = cross_val_score(model, x, y, cv=20)
-    print "Model mean accuracy: {}".format(numpy.mean(scores))
-
-    model.fit(x, y)
-    return model
-
-
-def export_model(model, out_path):
-    dot_data = StringIO.StringIO()
-    tree.export_graphviz(model, out_file=dot_data)
-    graph = pydot.graph_from_dot_data(dot_data.getvalue())
-    graph.write_pdf(out_path)
+def build_classifier(min_delta_imp=30, min_list_size=2, max_tree_node_amount=100):
+    """ Build classifier
+    :param min_delta_imp: (=30) split-stopping criteria
+    :param min_list_size: (=2) -||-
+    :return: Classifier
+    """
+    return MyClassifier(min_delta_imp, min_list_size, max_tree_node_amount)
 
 
 def parse_int(s):
@@ -80,23 +125,21 @@ def parse_string(s):
 
 
 def main():
-    print "## Welcome to the decision trees tutorial ##"
     args = parse_args()
 
-    columns = {
-        "age": (4, parse_int),
-        "gender": (5, parse_int),
-        "education": (29, parse_string),
-        TARGET: (54, parse_int)
-    }
-    x, y, feature_names = read_data_set(args.ds_path[0], columns)
+    print "parsed"
 
+    #VK
+    columns = choose_targets(2)
+    x, y, feature_names = read_data_set(columns=columns, ds_path='data/data.csv')
     print_set_stats(x, y, feature_names)
 
-    model = fit_decision_tree(x, y)
+    cls = build_classifier(30, 2, 100)
+    model = cls.fit(x, y)
+    model.draw_me(feature_names)
 
-    if args.out_path:
-        export_model(model, args.out_path)
+    model = cls.pruning(x[len(y)*8/10:], y[len(y)*8/10:], 1)
+    model.draw_me(feature_names, 'after_pruning')
 
 
 def parse_args():
